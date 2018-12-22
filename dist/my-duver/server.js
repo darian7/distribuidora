e869d61af274d7a035ff1ac26aceefecfba40a2c
Ubuntu <Darian@Yojan.fxtpsoe5a30ebawzyl232dp1jd.bx.internal.cloudapp.net>
2018-12-22 15:27:19 +0000

    servidor modificado

--- dist/my-duver/server.js
+++ dist/my-duver/server.js
@@ -4,7 +4,7 @@ const path = require ('path');
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4200;
 
 app.use(express.static(path.join(__dirname,'')));
 
